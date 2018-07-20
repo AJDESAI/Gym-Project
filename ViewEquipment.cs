77c57ff30bc5585a42554ccad17dd8b46582ce73
Ajit Desai <desaia20@gmail.com>
2018-07-20 13:17:06 -0700

    Add files via upload
    View the information related to the particular equipment, member, trainer and plans available in the Gym.

---
+++ ViewEquipment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+namespace WindowsFormsApplication2
+{
+    public partial class ViewEquipment : Form
+    {
+        OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Ajit\Documents\Visual Studio 2012\Projects\WindowsFormsApplication2\Database\gymManagement.accdb");
+        OleDbCommand cmd = new OleDbCommand();
+        public ViewEquipment()
+        {
+            InitializeComponent();
+        }
+
+        private void ViewEquipment_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                this.dataGridView1.DefaultCellStyle.Font = new Font("Lucida Bright", 11);
+                this.dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Lucida Bright", 13);
+                OleDbDataAdapter da = new OleDbDataAdapter("select * from equipment", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt.DefaultView;
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Error" + ex);
+            }
+        }
+    }
+}
