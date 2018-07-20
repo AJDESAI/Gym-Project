cb34a943e2ef05efa922394db762a46aed850e74
Ajit Desai <desaia20@gmail.com>
2018-07-20 13:23:21 -0700

    Contact_us
    Contact us module

---
+++ contact.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class contact : Form
+    {
+        public contact()
+        {
+            InitializeComponent();
+        }
+
+        private void contact_Load(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.Transparent;
+            label2.BackColor = Color.Transparent;
+            label3.BackColor = Color.Transparent;
+            label4.BackColor = Color.Transparent;
+            label5.BackColor = Color.Transparent;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+            
+        }
+    }
+}
